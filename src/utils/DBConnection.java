4aab97d48ce578d9f70cfbe4fa803010b2d75d5c
Amishka Gunasekaran <amishka@eminentbrands.com>
2025-06-14 21:26:11 +0530

    DB Change to file config.properties

--- src/utils/DBConnection.java
+++ src/utils/DBConnection.java
@@ -3,6 +3,7 @@ package utils;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 
@@ -10,15 +11,25 @@ public class DBConnection {
 
     public static Connection getConnection() throws Exception {
         Properties props = new Properties();
-        FileInputStream fis = new FileInputStream("config.properties");
-        props.load(fis);
-        fis.close();
+        System.out.println("Working dir: " + System.getProperty("user.dir"));
+        try (FileInputStream fis = new FileInputStream("config.properties")) {
+            props.load(fis);
+        } catch (Exception e){
+            System.err.println(e.getMessage());
+        }
 
         String url = props.getProperty("db.url");
         String user = props.getProperty("db.user");
         String pass = props.getProperty("db.password");
+        
 
-        return DriverManager.getConnection(url, user, pass);
+        try {
+            return DriverManager.getConnection(url, user, pass);
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return null;
     }
+    
 }
 
